0c2afd943ef64495b8aa2baa632c7f6aeb85e54d
MENNA123MAHMOUD <42938021+MENNA123MAHMOUD@users.noreply.github.com>
2020-03-18 02:58:10 +0200

    home footer

--- src/components/home-footer.vue
+++ src/components/home-footer.vue
@@ -1,26 +1,26 @@
 <template>
- <div>
-  <ul>
+  <div>
+    <ul>
       <li class="disabled">
-          Useful links
-     </li>
-      <router-link  to="/webplayer" tag="li">
-                WebPlayer
+        Useful links
+      </li>
+      <router-link to="/webplayer" tag="li">
+        WebPlayer
       </router-link>
-  </ul>
- </div>
+    </ul>
+  </div>
 </template>
 <style lang="scss" scoped>
 div {
- height: 400px;
- background-color: black;
- ul{
-     list-style-type: none;
-     li{
-         text-decoration: none;
-         color: white;
-     }
- }
+  height: 400px;
+  background-color: black;
+  ul {
+    list-style-type: none;
+    li {
+      text-decoration: none;
+      color: white;
+    }
+  }
 }
 </style>
 <script>
