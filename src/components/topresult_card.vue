1d5242c6066f987daf1da8e9b9d25c09156bc27c
MENNA123MAHMOUD <42938021+MENNA123MAHMOUD@users.noreply.github.com>
2020-04-07 14:27:09 +0200

    update top results card

--- src/components/topresult_card.vue
+++ src/components/topresult_card.vue
@@ -1,112 +1,174 @@
 <template>
-  <div class="col-Xs-4">
-    <h2 style="margin-bottom:15px;">TopResult</h2>
-    <div
-      class="card rounded col-lg-60%"
-      testid="top card"
-      @mouseover="hover=true"
-      @mouseleave="hover=false"
-      style="max-width: 400px;max-height: 250px;"
-    >
-      <div>
-        <img
-          v-if="type=='truk'"
-          :src="image"
-          class="col-md-4 img-card rounded-circle"
-          alt="..."
-          style="max-width: 40%;max-height: 200px; display:block; margin-left:0; margin-bottom:15px;"
-        />
-        <img
-          v-if="type=='Artist'"
-          :src="image"
-          class="col-md-4 img-card"
-          alt="..."
-          style="max-width: 40%;max-height: 100px; display:block; margin-left:0; margin-bottom:15px;"
-        />
-        <h2 class="card-title">{{name}}</h2>
-        <div>
-          <div class="typecont">
-            <p class="card-text p col-xs-4">{{type}}</p>
-          </div>
-          <div class="col-xs-4 play">
-            <router-link to="/" class="stretched-link" id="carglink" testid="cardlink"></router-link>
-            <i v-if="hover" class="fa fa-play-circle" testid="topplay icon"></i>
-          </div>
+    <div class="col-40%" id="top-card">
+        <h2 style="margin-bottom:15px;" class="col-lg-30%">TopResult</h2>
+        <div
+            class="card rounded col-lg-30%"
+            testid="top card"
+            @mouseover="hover = true"
+            @mouseleave="hover = false"
+            style="max-width:100%;"
+        >
+           <img
+                v-if="type == 'Artist'"
+                src="http://dummyimage.com/250x400.jpg/dddddd/000000"
+                testid="artistimage"
+                class="col-md-4 img-card rounded-circle"
+                alt="..."
+                style="max-width: 40%;max-height: 120px; display:block; margin-left:0; margin-bottom:15px;"
+            />
+            <img
+                v-if="type !== 'Artist'"
+                src="http://dummyimage.com/250x400.jpg/dddddd/000000"
+                class="col-md-4 img-card"
+                alt="..."
+                style="max-width: 40%;max-height: 100px; display:block; margin-left:0; margin-bottom:15px;"
+            />
+            <!-- <img
+                v-if="type == 'Artist'"
+                :src="image"
+                testid="artistimage"
+                class="col-md-4 img-card rounded-circle"
+                alt="..."
+                style="max-width: 40%;max-height: 120px; display:block; margin-left:0; margin-bottom:15px;"
+            />
+            <img
+                v-if="type !== 'Artist'"
+                :src="image"
+                class="col-md-4 img-card"
+                alt="..."
+                style="max-width: 40%;max-height: 100px; display:block; margin-left:0; margin-bottom:15px;"
+            /> -->
+            <div class="row">
+                <div class="col-8">
+                    <h2 class="card-title">{{ name }}</h2>
+                    <div class="typecont">
+                        <p class="card-text p" v-if="type == 'Artist'" testid="type" id="typecont">
+                            {{ type }}
+                        </p>
+                    </div>
+                </div>
+                <div class="playcon col-4" v-if="hover">
+                    <i class="fa fa-play-circle" testid="topplay icon"></i>
+                </div>
+            </div>
+            <router-link
+               v-if="type == 'Artist'"  
+                to="/"
+                class="stretched-link"
+                id="carglink"
+                testid="cardlink"
+            ></router-link>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 <style scoped>
-.play {
-  margin-right:5px;
-  margin-bottom: 200px;
+@media screen and (max-width: 700px) {
+  #top-card {
+    width:80%;
+  }
+  #typecont{
+    width:auto;
+    border-radius: 0px;
+  }
+  p{
+    margin: 0px;
+  }
+}
+@media screen and (max-width: 650px) {
+  #top-card {
+    width:70%;
+  }
+  #typecont{
+    width:auto;
+    border-radius: 0px;
+  }
+  i{
+    margin-left: 5px;
+  }
+  p{
+    font-size: 10px;
+    margin: 0px;
+  }
+}
+@media screen and (max-width: 500px) {
+  #top-card {
+    width:55%;
+  }
+  #typecont{
+    width:100%;
+  }
+}
+.playcon {
+    background-color: transparent;
+    float: right;
+    padding: 0mm;
 }
 .card-title {
-  margin-left: 10px;
-  margin-bottom: 10px;
+    margin-left: 10px;
+    margin-bottom: 10px;
 }
 .card-top {
-  background-color: #313030;
-  padding: 20px;
-  height: 250px;
+    background-color: #313030;
+    padding: 20px;
+    height: 250px;
 }
 .card {
-  display: block;
-  background: #313030;
-  width: 400px;
-  height: 270px;
-  margin-top: 15px;
-  z-index: 0;
-  padding: 20px;
-  margin-bottom: 9px;
+    display: block;
+    background: #313030;
+    width: 400px;
+    height: 270px;
+    margin-top: 15px;
+    z-index: 0;
+    padding: 20px;
+    margin-bottom: 9px;
 }
 h2 {
-  font-size: 28px;
-  font-weight: bold;
-  color: white;
-  margin-bottom: 5px;
+    font-size: 28px;
+    font-weight: bold;
+    color: white;
+    margin-bottom: 10px;
 }
 .p {
-  margin-left: 15px;
-  color: white;
-  margin-bottom: 15px;
+    margin-left: 10px;
+    color: white;
+    margin-bottom: 15px;
 }
 i {
-  margin-left: 200px;
-  margin-bottom: 150px;
-  color: #1db954;
-  position: relative;
-  font-size: 50px;
+    color: #1db954;
+    position: relative;
+    font-size: 50px;
+    margin-left:40%;
 }
 .typecont {
-  background-color: rgb(41, 32, 32);
-  border-radius: 15px;
-  width: 25%;
+    background-color: rgb(41, 32, 32);
+    border-radius: 15px;
+    width: 30%;
+    height: 25px;
+    float: left;
+    z-index: 0;
+    margin-left: 15px;
 }
 </style>
 <script>
 export default {
-  name: "top",
-  data: function() {
-    return {
-      hover: false
-    };
-  },
-  props: [
-    "image",
-    "name",
-    "type",
-    "artistId",
-    "artistName",
-    "albumId",
-    "playlist_id",
-    "track_id",
-    "ownerId",
-    "ownerName",
-    "Id"
-  ]
+    name: "top",
+    data: function() {
+        return {
+            hover: false,
+        };
+    },
+    props: [
+        "image",
+        "name",
+        "type",
+        "artistId",/*if artist to go his page*/
+        "artistName",/*if album*/
+        "albumId",/*to go to album page*/
+        "playlist_id",/*if playlist*/
+        "track_id",  
+        "ownerId",
+        "ownerName",/*if playlist*/
+        "Id",
+    ],
 };
-</script> 
-
-
+</script>
--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ if(process.env.NODE_ENV === "production"){
 //if in development call the mockServer
     mockServer();
 }
-axios.defaults.baseURL="http://52.205.254.29";
+axios.defaults.baseURL="https://spotify-demo1.herokuapp.com/";
 //setting the Authorization on axios header to our token, so requests can be processed if a token is required.
 // This way, we do not have to set token anytime we want to make a request.
 
