614fea9bfc3338074eeee0486edcc9c3b0ad805f
bassantt <bassantmohamed945@yahoo.com>
2020-06-09 03:30:45 +0200

    help&categoryplaylist&searchresultunittest

--- src/main.js
+++ src/main.js
@@ -12,8 +12,12 @@ Vue.config.productionTip = true;
 
 // axios.defaults.baseURL = "http://52.54.133.87";
 // Vue.prototype.$url="http://52.54.133.87"
-axios.defaults.baseURL = "http://52.87.106.56";
-Vue.prototype.$url="http://52.87.106.56"
+
+// axios.defaults.baseURL = "http://100.25.194.8";
+// Vue.prototype.$url="http://100.25.194.8"
+
+axios.defaults.baseURL = "http://52.70.203.73";
+Vue.prototype.$url="http://52.70.203.73";
 
 var vm = new Vue({
   router,
