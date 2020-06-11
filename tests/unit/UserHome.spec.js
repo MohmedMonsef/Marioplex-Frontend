82764be1167ade09491757694860cec547879ab5
MENNA123MAHMOUD <42938021+MENNA123MAHMOUD@users.noreply.github.com>
2020-06-11 13:20:06 +0200

    add master

--- tests/unit/UserHome.spec.js
+++ tests/unit/UserHome.spec.js
@@ -27,12 +27,14 @@ describe("UserHome", () => {
             POPnewreleases: {
               albums: [],
             },
+            loading:true
           },
           getters: {
             POPplaylists: (state) => state.POPplaylists,
             POPartists: (state) => state.POPartists,
             POPalbums: (state) => state.POPalbums,
             POPnewreleases: (state) => state.POPnewreleases,
+            loading: (state) => state.loading
           },
           actions: {
             showPopularPlaylists: jest.fn(),
